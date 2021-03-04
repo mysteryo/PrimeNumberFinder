f8b1fb5e501fb16c73f225fdd92d5c92b49d3244
fildagomez <fildagomez@seznam.cz>
2021-03-04 16:12:45 +0100

    Revisited + added comments

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -9,29 +9,43 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String path = args[0];
-        File file = new File(path);
+	public static void main(String[] args) {
+		//if argument were not typed in show console warning and close
+		if (args.length == 1) {
+			String path = args[0];
+			File file = new File(path);
+			//check if file exist
+			if (file.exists()) {
+				System.out.format("File %s exists\n", file.getAbsolutePath());
+				List<Double> numberArr;
+				System.out.println("Prime numbers are :");
+				try {
+					numberArr = ParseExcelFile.getParsedNumbers(path);
+					for (double number : numberArr) {
+						if (isNaturalNumber(number) && PrimeNumberResolver.isPrime(number)) {
+							System.out.println(number);
+						}
+					}
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			}
 
-        if (file.exists()) {
-            System.out.format("File %s exists\n", file.getAbsolutePath());
-            List<Integer> numberArr;
-            System.out.println("Prime numbers are :");
-            try {
-                numberArr = ParseExcelFile.getParsedNumbers(path);
-                for(int number : numberArr){
-                    if(number > 0 && PrimeNumberResolver.isPrime(number)){
-                        System.out.println(number);
-                    }
-                }
-            }
-            catch (IOException e){
-                System.out.println(e.toString());
-            }
+			else {
+				System.out.format("File %s does not exists", file.getAbsolutePath());
+			}
+		}
+		
+		else {
+			System.out.println("APPLICATION TAKES PARAMETER OF INPUT FILE PATH");
+		}
 
-        } else {
-            System.out.format("File %s does not exists", file.getAbsolutePath());
-        }
-
-    }
+	}
+	
+	//test if number is natural not negative or with decimal places
+	private static boolean isNaturalNumber(Double d) {
+		if(d % 1 > 0) return false;
+		if(d <= 0) return false;
+		return true;
+	}
 }
--- src/com/tutorial/calculations/PrimeNumberResolver.java
+++ src/com/tutorial/calculations/PrimeNumberResolver.java
@@ -3,9 +3,17 @@ package com.tutorial.calculations;
 public class PrimeNumberResolver {
 
 
-    public static boolean isPrime(int number){
+	
+    public static boolean isPrime(double number){
+    	//1 is not prime number
+    	if(number == 1) return false;
+    	if(number == 2 || number == 3) return true;
         boolean isPrime = true;
-        for (int i = 2; i < number/2; i++) {
+        //if we square root the number and remove decimal places we should get the highest nonrepeatable divider
+        //example square root of 63 is 7.9... therefore if we divide 63 with 2,3,4,5,6,7 we already got all combinations
+        //to decide if number is prime or not... numbers 2 and 3 don't work with this so just return true for them
+        int root = (int)Math.sqrt(number);
+        for (int i = 2; i <= root; i++) {
             if(number % i == 0){
                 isPrime = false;
                 break;
